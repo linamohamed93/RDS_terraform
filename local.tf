b4bf028753594200ae21bc2b0b101a0d97606e1f
linamohamed93 <96637850+linamohamed93@users.noreply.github.com>
2026-02-02 18:22:03 +0200

    Add files via upload

---
+++ local.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"  
+}
+
+module "create_rds" {
+  source = "C:/Users/lenovo/Desktop/RDS/rds module"
+
+ vpc_id="vpc-045aeda71ff7d5aa2"
+ vpc_cidr="172.31.0.0/16"
+ RDS_subnet_id=["subnet-07ea85f56b603d81f","subnet-04d32ad6ff76e6de6"]
+}
