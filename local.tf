8df7d5452279720d342c714356a11699a6f0eba4
linamohamed93 <96637850+linamohamed93@users.noreply.github.com>
2026-02-05 14:26:43 +0200

    Update local.tf

--- local.tf
+++ local.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 module "create_rds" {
-  source = "C:/Users/lenovo/Desktop/RDS/rds module"
+  source = "./RDS_module"
 
  vpc_id="vpc-045aeda71ff7d5aa2"
  vpc_cidr="172.31.0.0/16"
  RDS_subnet_id=["subnet-07ea85f56b603d81f","subnet-04d32ad6ff76e6de6"]
 }
+
